3edb3962c417c8e48789eb31b533524d65d6fa99
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-11-10 02:00:56 +0300

    set up the up to running production

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class ProdConfig(Config):
     """
     Production configuration class
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgres://vphupjjouxbchm:405e9885277306ea2a33321740801ee7a20f15e7d46524754f68e896f27fb4f6@ec2-54-146-84-101.compute-1.amazonaws.com:5432/dd4o6grtfa94e'
 
 
 class TestConfig(Config):
