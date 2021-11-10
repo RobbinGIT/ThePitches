e77be493e9343e668de21e6c77c48f37dd73c309
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-11-10 11:01:36 +0300

    exporting mail username

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'robbin.githimbo@student.moringaschool.com'
-    MAIL_PASSWORD = 'Shizzle27!'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 
 class ProdConfig(Config):
